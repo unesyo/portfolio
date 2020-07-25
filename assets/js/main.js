95d5fc3769dddc57137cfdf98fbda692077af027
unesyo <elhaddar.younes@gmail.com>
2020-07-25 19:14:31 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,11 +99,11 @@ jQuery(document).ready(function ($) {
 
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "99sbr");
+    new GitHubCalendar("#github-graph", "unesyo");
 
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "99sbr", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "unesyo", selector: "#ghfeed" });
 
 
 });
